b567ce67e198c4ab2ced411b55f6b45af9d255fb
shivmSingh1 <shivams7380@gmai.com>
2026-03-26 04:04:11 +0530

    tool calling setup

---
+++ invoke.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv"
+dotenv.config()
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main() {
+	const completion = await groq.chat.completions.create({
+		model: "llama-3.3-70b-versatile",
+		messages: [
+			{
+				role: "system",
+				content: "you are a personal assistant"
+			},
+			{
+				role: "user",
+				content: "who are you?",
+			},
+		],
+	});
+	console.log(completion.choices[0]?.message?.content);
+}
+
+
+main();
